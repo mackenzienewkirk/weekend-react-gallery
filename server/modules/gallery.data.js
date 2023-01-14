90b9af37f586c849223a65134722ac00169630ca
Mackenzie Newkirk <mackenzie.joy.newkirk@gmail.com>
2023-01-14 12:12:11 -0600

    all components are build, and starting to write code for each.

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,6 +1,10 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
-    { id: 2, path: 'images/selfie.jpeg', description: 'Selfie by the ocean.', likes: 0}
+    { id: 1, path: 'images/tree_base.jpeg', description: 'The base of a redwood tree.', likes: 0 },
+    { id: 2, path: 'images/selfie.jpeg', description: 'Selfie by the ocean.', likes: 0},
+    { id: 3, path: 'images/waves.jpeg', description: 'The Pacific ocean.', likes: 0},
+    { id: 4, path: 'images/elk_in_the_camp.jpeg', description: 'Elk came into the campsite.', likes: 0},
+    { id: 5, path: 'images/fern_canyon.jpeg', description: 'Fern canyon.', likes: 0},
+    { id: 6, path: 'images/redwoods.jpeg', description: 'The Redwood Forest.', likes: 0}
 ];
 
 module.exports = galleryItems;
