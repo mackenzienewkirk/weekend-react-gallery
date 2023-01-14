3e7ce5cf9fef0daaa613041737a04aa9ff97c4dd
Mackenzie Newkirk <mackenzie.joy.newkirk@gmail.com>
2023-01-14 12:52:44 -0600

    writing GET axios in app.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import axios from 'axios';
+import GalleryList from '../GalleryList/GalleryList';
+import GalleryItem from '../GalleryItem/GalleryItem';
+
 
 function App() {
+
+  const [galleryItems, setGalleryItems] = useState ([]);
+
+  useEffect(() => getPhotos(), []);
+
+  const getPhotos = () => {
+    axios({
+      method: 'GET',
+      url: '/gallery'
+    }).then(response => {
+      setGalleryItems(response.data);
+  }).catch(err => {
+    alert('error GETTING Gallery ');
+    console.log(err);
+  })
+}
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
         <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
-        <img src="images/selfie.jpeg"/>
+        <GalleryList 
+        items={galleryItems} 
+        getPhotos={getPhotos}
+        />
+        
+        {/* <img src="images/goat_small.jpg"/>
+        <img src="images/selfie.jpeg"/> */}
       </div>
     );
 }
